408e66c3297c8bef96a3c0358ae35ee234076be4
unknown <maivaldc2@gmail.com>
2021-11-23 14:07:03 -0500

    windows working

---
+++ woof/windows/woof/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::woof::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::woof::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
