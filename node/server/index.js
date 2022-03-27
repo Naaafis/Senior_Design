7ff4ad3ac0b8fd0df317641c5ad55ba8abe121fe
EC2 Default User <ec2-user@ip-10-0-0-190.us-east-2.compute.internal>
2022-03-27 19:03:46 +0000

    new server

--- node/server/index.js
+++ node/server/index.js
@@ -7,45 +7,68 @@ const db = require("mysql");
 app.use(bp.json())
 app.use(bp.urlencoded({extended: true}))
 
+let pool = db.createConnection({
+	host: "woof.cm3lizor7esf.us-east-2.rds.amazonaws.com",
+	user: "admin",
+	password: "monmon10!",
+	database: "woof"
+});
+
+pool.connect(function(err) {
+if (err) throw err;
+});
+
 app.get("/api", (req,res) => {
 	res.json({message: "Hello from server!"});
+});
+
+app.post("/receive_classification", (req, res) => {
+	console.log("Connected to MySQL");
+	var readdat = "SELECT username, prediction FROM datacollected WHERE username='"+req.body["dogname"]+"';";
+	pool.query(readdat, function(err, result) {
+	if(err) throw err;
+	res.json({message: result[0]["prediction"]});
+	});
+});
+app.post("/check_match", (req, res) => {
+	var check = "SELECT x.username, y.username FROM friends x, friends y WHERE x.username=y.pending AND x.pending=y.username;";
+	pool.query(check, function(err, result) {
+	if(err) throw err;
+	res.json({message: result[0]["friendslist"]});
+	});
+});
+app.post("/friends", (req,res) => {
+	res.json({friends: res.body});
+	console.log(req.body);
+	var add = "INSERT INTO friends(username,  pending) VALUES('"+req.body["username"]+"', '"+req.body["pending"]+"');";
+	pool.query(add, function(err, result) {
+	if(err) throw err;
+	console.log("Inserted");
+	});
 });
 app.post("/classification", (req, res) => {
 	res.json({classification: res.body});
 	console.log(req.body);
-	pool.connect(function(err) {
-	if(err) throw err;
 	console.log("Connected to MySQL");
-	var insdata = "INSERT INTO datacollected( x, y, z, microphone, gps, prediction) VALUES('"+req.body["x"]+"', '"+req.body["y"]+"', '"+req.body["z"]+"', '"+req.body["microphone"]+"', '"+req.body["gps"]+"', '"+req.body["prediction"]+"');";
+	var insdata = "INSERT INTO datacollected(username, x, y, z, microphone, gps, prediction) VALUES('"+req.body["username"]+"','"+req.body["x"]+"', '"+req.body["y"]+"', '"+req.body["z"]+"', '"+req.body["microphone"]+"', '"+req.body["gps"]+"', '"+req.body["prediction"]+"');";
 	pool.query(insdata, function(err, result) {
 	if(err) throw err;
 	console.log("inserted");
 	});
 });
-});
 app.post("/profile",(req,res) => {
 	res.send({message: "Received profile information. Thanks!"});
 	console.log(req.body);
-	pool.connect(function(err) {
-	if(err) throw err;
 	console.log("Connected to MySQL");
-	var insusers = "INSERT INTO users(userid, fullname, dob, email, password, dogname, dogbreed, abt,  datejoined) VALUES('"+req.body["userid"]+"', '"+req.body["fullname"]+"', '"+req.body["dob"]+"', '"+req.body["email"]+"', '"+req.body["password"]+"', '"+req.body["dogname"]+"', '"+req.body["dogbreed"]+"', '"+req.body["abt"]+"', '"+req.body["datejoined"]+"');";
+	var insusers = "INSERT INTO users(userid,username,  fullname, dob, pronouns,  email,  dogname, dogbreed, abt,  datejoined) VALUES('"+req.body["userid"]+"','"+req.body["username"]+"', '"+req.body["fullname"]+"', '"+req.body["dob"]+"','"+req.body["pronouns"]+"',  '"+req.body["email"]+"', '"+req.body["dogname"]+"', '"+req.body["dogbreed"]+"', '"+req.body["abt"]+"', CURDATE());";
 	pool.query(insusers, function(err, result) {
 	if(err) throw err;
 	console.log("inserted");
 	});
 });
-});
 
 app.listen(PORT, () => {
 	console.log('Server listenining on port 3000');
-});
-
-let pool = db.createConnection({
-	host: "woofs.cm3lizor7esf.us-east-2.rds.amazonaws.com",
-	user: "admin",
-	password: "monmon10!",
-	database: "woof"
 });
 
 
