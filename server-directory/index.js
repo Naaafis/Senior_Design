bd9c0354e5be0c10fb60c5db2b8aea376d2a94ea
Nafis Abeer <cheffbcookin@rfc1918.address.not.used.bu.edu>
2022-04-03 19:54:12 -0400

    new files

--- server-directory/index.js
+++ server-directory/index.js
@@ -35,4 +35,4 @@ app.post('/auth', (req, res) => {
   res.send(token);
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000'));
+app.listen(3000, '0.0.0.0'); //() => console.log('Server listening on port 3000'));
